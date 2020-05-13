7e4af2feb128eb6c44a764ae5945cbfbfab405f3
Ratatinator97 <asidiras.csi@gmail.com>
2020-05-13 23:56:24 +0200

    Heroku..PLS

--- dist/config/typeorm.config.js
+++ dist/config/typeorm.config.js
@@ -5,11 +5,11 @@ const config = require("config");
 const dbConfig = config.get('db');
 exports.typeOrmConfig = {
     type: dbConfig.type,
-    host: process.env.RDS_HOSTNAME || dbConfig.host,
+    host: process.env.DATABASE_URL || dbConfig.host,
     port: process.env.RDS_PORT || dbConfig.port,
-    username: process.env.RDS_USERNAME || dbConfig.username,
-    password: process.env.RDS_PASSWORD || dbConfig.password,
-    database: process.env.RDS_DB_NAME || dbConfig.database,
+    username: process.env.user || dbConfig.username,
+    password: process.env.password || dbConfig.password,
+    database: process.env.DB_NAME || dbConfig.database,
     entities: [__dirname + '/../**/*.entity.{js,ts}'],
     synchronize: process.env.TYPEORM_SYNC || dbConfig.synchronize,
 };
