34441ea5a30ec87853b523af46a8acaea518fd2f
Ratatinator97 <asidiras.csi@gmail.com>
2020-07-08 12:54:40 +0200

    Rechanged to ""

--- src/main.ts
+++ src/main.ts
@@ -27,7 +27,7 @@ async function bootstrap() {
   }
 
   const port = process.env.PORT || serverConfig.port;
-  await app.listen(port, '0.0.0.0');
+  await app.listen(port);
   logger.log(`Application listening on port ${port}`);
 }
 bootstrap();
